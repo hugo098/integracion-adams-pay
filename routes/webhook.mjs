040c3d92e21ff0ddd06c83de218665d9c2ca43d8
hugo098 <hi.rodriguez1998@gmail.com>
2021-07-21 12:10:54 -0400

    make it better

--- routes/webhook.mjs
+++ routes/webhook.mjs
@@ -9,7 +9,7 @@ router.post('/', async (req, res) => {
         console.log('WEBHOOK')
         //adams{"notify":{"id":"id-unico-de-notificacion","type":"(tipo)","version":1,"time":"hora de generación","merchant":"id-de-tu-comercio","app":"id-de-tu-aplicación","env":"ambiente"},"debt":{"docId":"id-de-deuda","objStatus":{"status":"active","time":"2020-05-02T13:51:16+00:00"},"payStatus":{"status":"pending","time":"2020-05-02T13:51:16+00:00"}}}a2ffb3633aee020e53336f6aa4384ce1
         //243eb6edccdf2e01f81698d407918c55
-        let hash = md5('adams' + JSON.parse(req.body) + 'a2ffb3633aee020e53336f6aa4384ce1')
+        let hash = md5('adams' + JSON.stringify(req.body) + 'a2ffb3633aee020e53336f6aa4384ce1')
         let hashReq = req.headers['x-adams-notify-hash']
         console.log(req);
         console.log(hash);
